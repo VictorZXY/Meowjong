f0625446fdb899211f5b2c151b8cd41e54b4cd10
Victor Zhao <victorzhao1999@gmail.com>
2020-10-29 18:31:47 +0000

    Create hand_config.py for hand score calculation

---
+++ hand_calculation/hand_config.py
@@ -0,0 +1,57 @@
+from hand_calculation.tile_constants import EAST
+from hand_calculation.yaku_config import YakuConfig
+
+
+class HandConfig:
+    """
+    Special class to pass various settings to the hand calculator object.
+    """
+    yaku = None
+
+    is_tsumo = False
+    is_riichi = False
+    is_ippatsu = False
+    is_rinshan = False
+    is_chankan = False
+    is_haitei = False
+    is_houtei = False
+    is_double_riichi = False
+    is_nagashi_mangan = False
+    is_tenhou = False
+    is_chiihou = False
+
+    player_wind = None
+    round_wind = None
+    is_dealer = False
+
+    def __init__(self,
+                 is_tsumo=False,
+                 is_riichi=False,
+                 is_ippatsu=False,
+                 is_rinshan=False,
+                 is_chankan=False,
+                 is_haitei=False,
+                 is_houtei=False,
+                 is_double_riichi=False,
+                 is_nagashi_mangan=False,
+                 is_tenhou=False,
+                 is_chiihou=False,
+                 player_wind=None,
+                 round_wind=None):
+        self.yaku = YakuConfig()
+
+        self.is_tsumo = is_tsumo
+        self.is_riichi = is_riichi
+        self.is_ippatsu = is_ippatsu
+        self.is_rinshan = is_rinshan
+        self.is_chankan = is_chankan
+        self.is_haitei = is_haitei
+        self.is_houtei = is_houtei
+        self.is_double_riichi = is_double_riichi
+        self.is_nagashi_mangan = is_nagashi_mangan
+        self.is_tenhou = is_tenhou
+        self.is_chiihou = is_chiihou
+
+        self.player_wind = player_wind
+        self.round_wind = round_wind
+        self.is_dealer = (player_wind == EAST)
