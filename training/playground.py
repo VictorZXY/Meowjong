d83afe9374fd048932e651fb8c85ad873331b825
Victor Zhao <victorzhao1999@gmail.com>
2021-01-16 01:54:49 +0800

    Enable different kernel size options for the CNN model

--- training/playground.py
+++ training/playground.py
@@ -1,18 +1,57 @@
 import os
-import pickle
 import numpy as np
-
+import pandas as pd
+import tensorflow as tf
 from PIL import Image
+from sklearn.model_selection import train_test_split
+from tensorflow import keras
 
-from data_processing.data_preprocessing_constants import TOTAL_FEATURES_COUNT, \
-    TOTAL_COLUMNS_SIZE
+from data_processing.data_preprocessing_constants import DATASET_PATH
+
+assert tf.__version__ >= "2.0"
+
+# To make the output stable across runs
+np.random.seed(42)
+tf.random.set_seed(42)
+
+PON_COUNT_2019 = {
+    'total': 101020,
+    'yes': 28462,
+    'no': 72558
+}
+AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 
-def to_array(binary):
-    return np.array([int(item) for item in
-                     list(bin(binary)[2:].zfill(TOTAL_FEATURES_COUNT))]) \
-        .reshape((34, TOTAL_COLUMNS_SIZE)).astype(np.uint8) * 255
+def load_data(csv_file):
+    csv_path = os.path.join(DATASET_PATH, csv_file)
+    return pd.read_csv(csv_path, sep=',')
+
+
+image_files = tf.data.Dataset.list_files(
+    os.path.join(DATASET_PATH, 'pon_2019/*.png'))
+labels = load_data('pon_actions_2019.csv')
+
+
+def get_label(image_path):
+    image_name = image_path.numpy().decode('utf-8').split('\\')[-1]
+    image_index = int(image_name[9:-4])
+    return tf.constant(labels['label'][image_index - 1])
+
+
+def decode_image(image):
+    image = tf.image.decode_png(image, channels=1)  # grayscale
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    return image
+
+
+def process_path(file_path):
+    label = get_label(file_path)
+    image = tf.io.read_file(file_path)
+    image = decode_image(image)
+    return image, label
 
 
 if __name__ == '__main__':
-    print('Hello World!')
+    X, y = image_files.map(process_path, num_parallel_calls=AUTOTUNE)
+    print(X)
+    print(y)
--- training/pon_model.py
+++ training/pon_model.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import pickle
+from datetime import datetime
 from functools import partial
 
 import numpy as np
@@ -8,105 +9,176 @@ import pandas as pd
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from tensorflow import keras
-from tqdm import tqdm
 
 assert tf.__version__ >= "2.0"
-
-# Parse the args
-parser = argparse.ArgumentParser()
-parser.add_argument('--dataset_path', action='store', type=str, required=True)
-parser.add_argument('--cnn_path', action='store', type=str, required=True)
-
-args = parser.parse_args()
-dataset_path = args.dataset_path
-cnn_path = args.cnn_path
 
 # To make the output stable across runs
 np.random.seed(42)
 tf.random.set_seed(42)
 
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
 TOTAL_FEATURES_COUNT = 12410
 TOTAL_COLUMNS_SIZE = 365
+PON_COUNT_2019 = {
+    'total': 101020,
+    'yes': 28462,
+    'no': 72558
+}
 
 
-def to_array(binary):
-    return np.array([int(item) for item in
-                     list(bin(binary)[2:].zfill(TOTAL_FEATURES_COUNT))]) \
-        .reshape((34, TOTAL_COLUMNS_SIZE)).astype(float)
+def kernel(arg):
+    try:
+        x, y = map(int, arg.split(','))
+        return x, y
+    except Exception:
+        raise argparse.ArgumentTypeError("Kernel size must be x, y")
 
 
-pon_data = pd.DataFrame(columns=['state', 'target'])
+def validate_dataset(dataset_path):
+    path, dirs, files = next(os.walk(os.path.join(dataset_path, 'pon_2019')))
+    assert len(files) == PON_COUNT_2019['total']
 
-with tqdm(desc='Decoding', total=453590) as pbar:
-    action_flag = False
-    for i in range(20):
-        filename = os.path.join(dataset_path,
-                                'pon_2019_{:0>2d}'.format(i + 1) + '.pickle')
-        with open(filename, 'rb') as fread:
-            try:
-                while True:
-                    data = pickle.load(fread)
-                    if action_flag:
-                        target = to_array(data)
-                        pon_data = pon_data.append(
-                            {'state': state, 'target': target},
-                            ignore_index=True)
-                        pbar.update(1)
-                    else:
-                        state = to_array(data)
-                    action_flag = not action_flag
-            except EOFError:
-                pass
+    with open(os.path.join(dataset_path, 'pon_actions_2019.csv')) as f:
+        f.readline()
+        assert sum(1 for line in f) == PON_COUNT_2019['total']
+        f.seek(0)
+        f.readline()
+        assert sum(int(line[-2]) for line in f) == PON_COUNT_2019['yes']
 
-X, y = np.stack(np.array(pon_data['state'])), pon_data['target']
-X_train, X_dev, y_train, y_dev = train_test_split(X, y, test_size=0.1,
-                                                  train_size=0.9,
-                                                  random_state=42)
-print('X_train shape:', X_train.shape)
-print('X_dev.shape:', X_dev.shape)
-print('y_train shape:', y_train.shape)
-print('y_dev.shape:', y_dev.shape)
 
-DefaultConv2D = partial(keras.layers.Conv2D, kernel_size=3, activation='relu',
-                        padding="VALID")
+def load_data(csv_file):
+    csv_path = os.path.join(dataset_path, csv_file)
+    return pd.read_csv(csv_path, sep=',')
 
-tf.keras.backend.clear_session()
 
-model = keras.models.Sequential([
-    DefaultConv2D(filters=128, input_shape=[34, 365, 1]),
-    keras.layers.BatchNormalization(),
-    keras.layers.Dropout(0.5),
-    DefaultConv2D(filters=128),
-    keras.layers.BatchNormalization(),
-    keras.layers.Dropout(0.5),
-    DefaultConv2D(filters=128),
-    keras.layers.BatchNormalization(),
-    keras.layers.Dropout(0.5),
-    keras.layers.Flatten(),
-    keras.layers.Dense(units=512, activation='relu'),
-    keras.layers.BatchNormalization(),
-    keras.layers.Dropout(0.5),
-    keras.layers.Dense(units=512, activation='relu'),
-    keras.layers.BatchNormalization(),
-    keras.layers.Dropout(0.5),
-    keras.layers.Dense(units=2, activation='softmax'),
-])
+def get_label(image_file, labels):
+    # image_name = image_file.numpy().decode('utf-8').split('\\')[-1]  # Windows
+    image_name = image_file.numpy().decode('utf-8').split('/')[-1]  # Linux
+    image_index = int(image_name[9:-4])
+    return tf.constant(labels['label'][image_index - 1])
 
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
 
-history = model.fit(X_train, y_train, batch_size=32, epochs=500,
-                    validation_data=(X_dev, y_dev))
+def decode_image(image):
+    image = tf.image.decode_png(image, channels=1)  # grayscale
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    return image
 
-with open(os.path.join(cnn_path, 'history.pickle'), 'wb') as f_history:
-    pickle.dump(history, f_history)
 
-model.save(os.path.join(cnn_path, 'pon_model_01.h5'))
+def generate_state_action_pair(image_file, labels):
+    label = get_label(image_file, labels)
+    image = tf.io.read_file(image_file)
+    image = decode_image(image)
+    return image, label
 
-eval_train = model.evaluate(X_train, y_train)
-print('final training loss:', eval_train[0])
-print('final training accuracy:', eval_train[1])
-eval_dev = model.evaluate(X_dev, y_dev)
-print('final dev loss:', eval_dev[0])
-print('final dev accuracy:', eval_dev[1])
+
+def create_model(kernel_size):
+    DefaultConv2D = partial(keras.layers.Conv2D, kernel_size=kernel_size,
+                            activation='relu',
+                            padding="VALID")
+    return keras.models.Sequential([
+        DefaultConv2D(filters=128, input_shape=[34, 365, 1]),
+        keras.layers.BatchNormalization(),
+        keras.layers.Dropout(0.5),
+        DefaultConv2D(filters=128),
+        keras.layers.BatchNormalization(),
+        keras.layers.Dropout(0.5),
+        DefaultConv2D(filters=128),
+        keras.layers.BatchNormalization(),
+        keras.layers.Dropout(0.5),
+        keras.layers.Flatten(),
+        keras.layers.Dense(units=512, activation='relu'),
+        keras.layers.BatchNormalization(),
+        keras.layers.Dropout(0.5),
+        keras.layers.Dense(units=256, activation='relu'),
+        keras.layers.BatchNormalization(),
+        keras.layers.Dropout(0.5),
+        keras.layers.Dense(units=2, activation='softmax'),
+    ])
+
+
+if __name__ == '__main__':
+    # Parse the args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_path', action='store', type=str,
+                        required=True)
+    parser.add_argument('--logs_path', action='store', type=str, required=True)
+    parser.add_argument('--cnn_path', action='store', type=str, required=True)
+    parser.add_argument('--kernel_size', action='store', type=kernel,
+                        required=True)
+
+    args = parser.parse_args()
+    dataset_path = args.dataset_path
+    logs_path = args.logs_path
+    cnn_path = args.cnn_path
+    kernel_size = args.kernel_size
+
+    # Load and validate the dataset
+    validate_dataset(dataset_path)
+
+    image_files = tf.data.Dataset.list_files(os.path.join(dataset_path,
+                                                          'pon_2019/*.png'))
+    labels = load_data('pon_actions_2019.csv')
+    for i in range(len(labels)):
+        assert labels['image'][i] == 'pon_2019_' + str(i + 1) + '.png'
+    print('Dataset OK')
+    print()
+
+    # Generate (state, action) (i.e. (image, label)) pairs
+    X = []
+    y = []
+
+    for file in image_files:
+        image, label = generate_state_action_pair(file, labels)
+        X.append(image)
+        y.append(label)
+
+    X_train, X_dev, y_train, y_dev = train_test_split(X, y, test_size=0.1,
+                                                      train_size=0.9,
+                                                      random_state=42,
+                                                      stratify=y)
+    X_train = tf.stack(X_train)
+    X_dev = tf.stack(X_dev)
+    y_train = tf.stack(y_train)
+    y_dev = tf.stack(y_dev)
+    print('X_train shape:', X_train.shape)
+    print('X_dev.shape:', X_dev.shape)
+    print('y_train shape:', y_train.shape)
+    print('y_dev.shape:', y_dev.shape)
+    print()
+
+    # Create neural network
+    tf.keras.backend.clear_session()
+
+    model = create_model(kernel_size)
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    print(model.summary())
+    print()
+
+    # Train the neural network
+    model_name = 'pon_cnn_' + str(kernel_size[0]) + str(kernel_size[1])
+    log_dir = os.path.join(logs_path + '/' + model_name,
+                           datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir,
+                                                          histogram_freq=1)
+
+    history = model.fit(X_train, y_train, batch_size=32, epochs=500,
+                        validation_data=(X_dev, y_dev),
+                        callbacks=[tensorboard_callback])
+
+    # Save the neural network
+    with open(os.path.join(cnn_path, model_name + '_history.pickle'), 'wb') \
+            as f_history:
+        pickle.dump(history, f_history)
+
+    model.save(os.path.join(cnn_path, model_name + '.h5'))
+
+    eval_train = model.evaluate(X_train, y_train)
+    print('final training loss:', eval_train[0])
+    print('final training accuracy:', eval_train[1])
+    eval_dev = model.evaluate(X_dev, y_dev)
+    print('final dev loss:', eval_dev[0])
+    print('final dev accuracy:', eval_dev[1])
