ec38afabf5c94a61dbadd197934de030588d20c6
Victor Zhao <victorzhao1999@gmail.com>
2021-03-01 16:44:49 +0800

    UPDATE random_agent.py

--- evaluation/agents/random_agent.py
+++ evaluation/agents/random_agent.py
@@ -5,24 +5,33 @@ from evaluation.hand_calculation.tiles import Tiles
 
 
 class RandomAgent(Agent):
-    def eval_discard(self, target_tile):
+    def eval_discard(self, target_tile, player1, player2, player3,
+                     scores, round_number, honba_number, deposit_number,
+                     dora_indicators):
         return np.random.choice(Tiles.matrices_to_array(self.hand)
                                 + [target_tile])
 
-    def eval_pon(self, target_tile):
-        return np.random.uniform(0.0, 1.0)
+    def eval_pon(self, target_tile, player1, player2, player3,
+                 scores, round_number, honba_number, deposit_number,
+                 dora_indicators):
+        return np.random.choice([0.0, 1.0])
 
-    def eval_kan(self, target_tile):
-        return np.random.uniform(0.0, 1.0)
+    def eval_kan(self, target_tile, player1, player2, player3,
+                 scores, round_number, honba_number, deposit_number,
+                 dora_indicators):
+        return np.random.choice([0.0, 1.0])
 
-    def eval_kita(self, target_tile):
-        return np.random.uniform(0.0, 1.0)
+    def eval_kita(self, target_tile, player1, player2, player3,
+                  scores, round_number, honba_number, deposit_number,
+                  dora_indicators):
+        return np.random.choice([0.0, 1.0])
 
-    def eval_riichi(self, target_tile):
-        return np.random.uniform(0.0, 1.0)
+    def eval_riichi(self, target_tile, player1, player2, player3,
+                    scores, round_number, honba_number, deposit_number,
+                    dora_indicators):
+        return np.random.choice([0.0, 1.0])
 
-    def eval_kyuushu_kyuuhai(self, target_tile):
-        return np.random.uniform(0.0, 1.0)
-
-    def eval_win(self, target_tile):
-        return 1.0
+    def eval_kyuushu_kyuuhai(self, target_tile, player1, player2, player3,
+                             scores, round_number, honba_number, deposit_number,
+                             dora_indicators):
+        return np.random.choice([0.0, 1.0])
