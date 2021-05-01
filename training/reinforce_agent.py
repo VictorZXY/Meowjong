c677321e997284e3ff4dabbc8362099f15e96b0c
Victor Zhao <victorzhao1999@gmail.com>
2021-05-01 12:30:39 +0800

    Skip problematic seeds

--- training/reinforce_agent.py
+++ training/reinforce_agent.py
@@ -347,7 +347,7 @@ if __name__ == '__main__':
     seed = 0
     while eps < episodes:
         try:
-            if seed == 341:
+            if seed == 341 or seed == 484:
                 seed += 1
                 continue
 
