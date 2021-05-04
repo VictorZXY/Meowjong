b39143fdf544cf7e5429f0449e17f1f5e9ad3d8e
Victor Zhao <victorzhao1999@gmail.com>
2021-05-05 02:36:12 +0800

    Update results analysis

--- evaluation/results_analysis/reinforce_analysis.py
+++ evaluation/results_analysis/reinforce_analysis.py
@@ -6,11 +6,14 @@ CSV_FILENAME = '..\\..\\results\\REINFORCE-evaluation\\REINFORCE-evaluation.csv'
 FIELD_NAMES = [
     'eps',
     'illegal_count',
-    'loss_count',
     'east_win_rate',
     'south_win_rate',
     'west_win_rate',
     'total_win_rate',
+    'east_loss_rate',
+    'south_loss_rate',
+    'west_loss_rate',
+    'total_loss_rate',
     'east_avg_score',
     'south_avg_score',
     'west_avg_score',
@@ -26,10 +29,12 @@ if __name__ == '__main__':
     with open(CSV_FILENAME, 'w', newline='') as csvfile:
         csv_writer = csv.DictWriter(csvfile, fieldnames=FIELD_NAMES)
         csv_writer.writeheader()
+
         for eps in range(40):
             filename = str(eps + 1) + '.txt'
             # 0: East, 1: South, 2: West, 3: total
             win_scores = [[], [], [], []]
+            loss_scores = [[], [], [], []]
             illegal_count = 0
             loss_count = 0
 
@@ -43,19 +48,24 @@ if __name__ == '__main__':
                         else:
                             data = list(map(int, line))
                             if data[wind + 1] < 0:
-                                loss_count += 1
+                                loss_scores[wind].append(data[wind + 1])
                             elif data[wind + 1] > 0:
                                 win_scores[wind].append(data[wind + 1])
+
             csv_writer.writerow({
                 'eps': (eps + 1) * 10,
                 'illegal_count': illegal_count,
-                'loss_count': loss_count,
-                'east_win_rate': len(win_scores[0]) / 500,
-                'south_win_rate': len(win_scores[1]) / 500,
-                'west_win_rate': len(win_scores[2]) / 500,
-                'total_win_rate': (len(win_scores[0]) + len(win_scores[1]) + len(win_scores[2])) / 1500,
-                'east_avg_score': sum(win_scores[0]) / len(win_scores[0]),
-                'south_avg_score': sum(win_scores[1]) / len(win_scores[1]),
-                'west_avg_score': sum(win_scores[2]) / len(win_scores[2]),
-                'total_avg_score': (sum(win_scores[0]) + sum(win_scores[1]) + sum(win_scores[2])) / (len(win_scores[0]) + len(win_scores[1]) + len(win_scores[2]))
+                'east_win_rate': len(win_scores[0]) / (500 - illegal_count),
+                'south_win_rate': len(win_scores[1]) / (500 - illegal_count),
+                'west_win_rate': len(win_scores[2]) / (500 - illegal_count),
+                'total_win_rate': (len(win_scores[0]) + len(win_scores[1]) + len(win_scores[2])) / (1500 - 3 * illegal_count),
+                'east_loss_rate': len(loss_scores[0]) / (500 - illegal_count),
+                'south_loss_rate': len(loss_scores[1]) / (500 - illegal_count),
+                'west_loss_rate': len(loss_scores[2]) / (500 - illegal_count),
+                'total_loss_rate': (len(loss_scores[0]) + len(loss_scores[1]) + len(loss_scores[2])) / (1500 - 3 * illegal_count),
+                'east_avg_score': (sum(win_scores[0]) + sum(loss_scores[0])) / (len(win_scores[0]) + len(loss_scores[0])),
+                'south_avg_score': (sum(win_scores[1]) + sum(loss_scores[1])) / (len(win_scores[1]) + len(loss_scores[1])),
+                'west_avg_score': (sum(win_scores[2]) + sum(loss_scores[2])) / (len(win_scores[2]) + len(loss_scores[2])),
+                'total_avg_score': (sum(win_scores[0]) + sum(win_scores[1]) + sum(win_scores[2]) + sum(loss_scores[0]) + sum(loss_scores[1]) + sum(loss_scores[2]))
+                                   / (len(win_scores[0]) + len(win_scores[1]) + len(win_scores[2]) + len(loss_scores[0]) + len(loss_scores[1]) + len(loss_scores[2]))
             })
